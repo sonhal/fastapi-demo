788c651d8c0de5de9f0d7ac2f571cfd79b209369
Sondre Halvorsen <sondre.hal@gmail.com>
2020-03-13 00:26:14 +0100

    CREATE domain object test

---
+++ tests/competitors/test_competition.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class TestCompetition(TestCase):
+
+    def test_register_competitor(self):
+        self.fail()
+
+    def test_next_outbound_challenge(self):
+        self.fail()
